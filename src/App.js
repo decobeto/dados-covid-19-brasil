d69ec8965c8e7b8cfaf905139d690d8dbc916ea3
André Roberto <andreroberto52@gmail.com>
2021-03-15 19:11:22 -0300

    Alterações

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import styled from 'styled-components'
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 import './styles/global.css'
-import axios from 'axios'
-
-const api = axios.create({
-  baseURL: "https://servicodados.ibge.gov.br",
-});
-
-api.get('/api/v3/agregados/1613/periodos/2019/variaveis/2313?localidades=N3[42]&classificacao=82[all]').then( response => console.log(response.data))
 
 function App() {
+  const [information, setInformation] = useState()
+  useEffect(() => {
+    api.get('/api/v3/agregados/1613/periodos/2019/variaveis/2313?localidades=N3[42]&classificacao=82[all]').then( response => setInformation(response.data))
+  }, [setInformation]) 
+
   return (
-    <h1>hello </h1>
+    <>
+    
+    {/* {information.map(info => info.id)} */}
+    </>
   );
 }
 
