1840eeb6e3d65f745e0d089a3c5a2910002a1890
André Roberto <andreroberto52@gmail.com>
2021-03-15 21:42:38 -0300

    Adição da pasta de components e da navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import Navbar from './components/navBar';
 import api from './services/api';
 import './styles/global.css'
 
 const Section = styled.section`
+  height: 100vh;
   background-color: #F2F3F5;
 `
 
 function App() {
-  const [information, setInformation] = useState()
+  const [data, setData] = useState()
   useEffect(() => {
     async function fetchData(){
       
       const request = await api.get('/api/v3/agregados/1613/periodos/2019/variaveis/2313%7C1002313%7C216%7C1000216%7C214%7C112%7C215%7C1000215?localidades=N3[42]&classificacao=82[all]')
-      console.log(request)
+      setData(request.data)
+      return request
     }
     fetchData()
   }, []) 
 
+  console.log(data)
+
   return (
     <>
       <Section>
-
+        <Navbar />
       </Section>
     </>
   );
---
+++ src/components/navBar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import '../styles/global.css'
+
+const Nav = styled.nav`
+  flex: 1;
+  display: flex;
+  background-color: #6c5ce7;
+  height: 60px;
+  box-shadow: 1px -7px 40px 9px rgba(0,0,0,0.75);
+  -webkit-box-shadow: 1px -7px 40px 9px rgba(0,0,0,0.75);
+  -moz-box-shadow: 1px -7px 40px 9px rgba(0,0,0,0.75);
+`
+
+const Container = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: start;
+`
+
+const SiteName = styled.li`
+  list-style-type: none;
+  margin-left: 15px;
+  font-weight: 700;
+  color: white;
+  font-size: 2rem;
+  font-family: 'Roboto';
+`
+
+
+export default function Navbar(){
+  return(
+    <Nav>
+      <Container>
+        <SiteName>🌾Produção Agrícola Municipal</SiteName>
+      </Container>
+    </Nav>
+  )
+}
