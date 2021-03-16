c4142d150ff8b005b18267de2e11d49e3ee86ab0
André Roberto <andreroberto52@gmail.com>
2021-03-15 21:10:11 -0300

    Pegando dados da api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import api from './services/api';
 import './styles/global.css'
+
+const Section = styled.section`
+  background-color: #F2F3F5;
+`
 
 function App() {
   const [information, setInformation] = useState()
   useEffect(() => {
-    api.get('/api/v3/agregados/1613/periodos/2019/variaveis/2313?localidades=N3[42]&classificacao=82[all]').then( response => setInformation(response.data))
-  }, [setInformation]) 
+    async function fetchData(){
+      
+      const request = await api.get('/api/v3/agregados/1613/periodos/2019/variaveis/2313%7C1002313%7C216%7C1000216%7C214%7C112%7C215%7C1000215?localidades=N3[42]&classificacao=82[all]')
+      console.log(request)
+    }
+    fetchData()
+  }, []) 
 
   return (
     <>
-    
-    {/* {information.map(info => info.id)} */}
+      <Section>
+
+      </Section>
     </>
   );
 }
