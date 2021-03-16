b617bdd5fabeae460da20cee76a9646d6a1c676a
André Roberto <andreroberto52@gmail.com>
2021-03-15 23:32:16 -0300

    Adição de select e mudança na chamada da api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, {  useState, useCallback } from 'react';
 import styled from 'styled-components';
+import Select from 'react-select'
 import Navbar from './components/navBar';
 import api from './services/api';
 import './styles/global.css'
+import { period, state } from './services/selectOptions';
 
 const Section = styled.section`
   height: 100vh;
@@ -11,22 +13,31 @@ const Section = styled.section`
 
 function App() {
   const [data, setData] = useState()
-  useEffect(() => {
-    async function fetchData(){
-      
-      const request = await api.get('/api/v3/agregados/1613/periodos/2019/variaveis/2313%7C1002313%7C216%7C1000216%7C214%7C112%7C215%7C1000215?localidades=N3[42]&classificacao=82[all]')
-      setData(request.data)
-      return request
-    }
-    fetchData()
-  }, []) 
+  const [selectedPeriod, setSelectedPeriod] = useState('')
+  const [selectedState, setSelectedState] = useState('')
+  const [isSending, setIsSending] = useState(false)
+  
+  const sendRequest = useCallback(async (selectedPeriod, selectedState) => {
+    if (isSending) return
+    setIsSending(true)
+    const request = await api.get(`/api/v3/agregados/1613/periodos/${selectedPeriod.value}/variaveis/2313%7C1002313%7C216%7C1000216%7C214%7C112%7C215%7C1000215?localidades=N3[${selectedState.value}]&classificacao=82[all]`)
+    setData(request.data)
+    setIsSending(false)
+  }, [isSending])
 
   console.log(data)
+  console.log(selectedState.value)
+  console.log(selectedPeriod.value)
 
   return (
     <>
       <Section>
         <Navbar />
+        <div>
+          <Select options={period} value={selectedPeriod} onChange={setSelectedPeriod} />
+          <Select options={state} value={selectedState} onChange={setSelectedState} />
+          <input value='Aperta eu' type={"button"} disabled={isSending} onClick={sendRequest} />
+        </div>
       </Section>
     </>
   );
